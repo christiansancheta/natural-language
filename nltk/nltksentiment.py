fe804a4bf5054c439937e13bdba72d6ea445f7f2
Christian Ancheta <christian.s.ancheta@gmail.com>
2022-12-30 05:25:06 -0500

    gensim classifier

--- nltk/nltksentiment.py
+++ nltk/nltksentiment.py
@@ -7,6 +7,6 @@ def get_sentiment(text):
     scores = analyzer.polarity_scores(text)
     return scores['compound']
 
-text = "" # Replace this with the string you want to analyze
+text = ""
 sentiment = get_sentiment(text)
 print(sentiment)
